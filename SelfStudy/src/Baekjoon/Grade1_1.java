4d01c678c4926fe3428ac27cc174b9bae87be26c
EunKyung Shin <heybrilliante@gmail.com>
2021-06-08 16:11:12 +0900

    Grade1_오타수정
    오타 수정

---
+++ SelfStudy/src/Baekjoon/Grade1_1.java
@@ -0,0 +1,10 @@
+package Baekjoon;
+
+// �ܰ� 1.����°� ��Ģ����
+// ����1 : Hello World!�� ����Ͻÿ�.
+
+public class Grade1_1 { // ���� ���ؿ��� class���� Main���� ����
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ SelfStudy/src/Baekjoon/Grade1_2.java
@@ -0,0 +1,11 @@
+package Baekjoon;
+
+//�ܰ� 1.����°� ��Ģ����
+//����2 : �� �ٿ� ���� "����ģ�� ��������"�� �� �ٿ� �� ���� ����Ѵ�.
+
+public class Grade1_2 { // ���� ���ؿ��� class���� Main���� ����
+	public static void main(String[] args) {
+		System.out.println("����ģ�� ��������");
+		System.out.println("����ģ�� ��������");
+	}
+}
