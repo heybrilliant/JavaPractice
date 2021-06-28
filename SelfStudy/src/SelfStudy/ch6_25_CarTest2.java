4af3995a7cb75e2d1731fa9bc70b853073da749c
EunKyung Shin <heybrilliante@gmail.com>
2021-06-29 01:33:58 +0900

    Update ch6_25_CarTest2.java
    210629 this() 생성자 변수 수정(자바의 정석)

--- SelfStudy/src/SelfStudy/ch6_25_CarTest2.java
+++ SelfStudy/src/SelfStudy/ch6_25_CarTest2.java
@@ -1,19 +1,19 @@
 package SelfStudy;
 
-class Car{
+class Car2{
 	String color;		// 색상
 	String gearType;	// 변속기 종류 - auto 자동, manual 수동
 	int door;			// 문의 개수 
 
-	Car() {
+	Car2() {
 		this("white", "auto", 4); // Car(String color, String gearType, int door)를 호출
 	}
 	
-	Car(String color) {
+	Car2(String color) {
 		this(color, "auto", 4); // color
 	}
 	
-	Car(String color, String gearType, int door) {
+	Car2(String color, String gearType, int door) {
 		this.color = color;
 		this.gearType = gearType;
 		this.door = door;
@@ -21,11 +21,9 @@ class Car{
 }	
 
 public class ch6_25_CarTest2 {
-
 	public static void main(String[] args) {
-		Car c1 = new Car();  	
-		Car c2 = new Car("blue"); 
-
+		Car2 c1 = new Car2();  	
+		Car2 c2 = new Car2("blue"); 
 		
 		System.out.println("c1의 color = "+ c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
 		System.out.println("c2의 color = "+ c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
