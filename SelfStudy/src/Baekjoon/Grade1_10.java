03ecb18796c3a2fb8788ae9562d65b8a7d5b9fc9
EunKyung Shin <heybrilliante@gmail.com>
2021-07-18 23:41:53 +0900

    Grade1_10 (10430)
    Grade1_10 (10430)

---
+++ SelfStudy/src/Baekjoon/Grade1_10.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+//단계1.입출력과 사칙연산
+//문제9: (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+// (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+// 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+//백준 입력시 class이름은 Main
+
+public class Grade1_10 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C) * (B%C))%C);
+		
+	}
+}
