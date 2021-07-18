a722d8b5b8c4d01be2a70cec90629e219c6fc4ed
EunKyung Shin <heybrilliante@gmail.com>
2021-07-18 23:44:03 +0900

    Grade1_10 (10430)
    Grade1_10 (10430) + 사족수정

--- SelfStudy/src/Baekjoon/Grade1_10.java
+++ SelfStudy/src/Baekjoon/Grade1_10.java
@@ -3,7 +3,7 @@ package Baekjoon;
 import java.util.Scanner;
 
 //단계1.입출력과 사칙연산
-//문제9: (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+//문제10: (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
 // (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
 // 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
 //백준 입력시 class이름은 Main
